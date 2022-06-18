4100dfa3e06733c02905ac11bc333794448f31c4
jnsbtks <69842196+jnsbtks@users.noreply.github.com>
2022-06-18 14:17:26 +0300

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -2,7 +2,7 @@ import sys
 from twocaptcha import TwoCaptcha
 
 image = sys.argv[1]
-casesensitive = sys.argv[2]
+### casesensitive = sys.argv[2]
 
 solver = TwoCaptcha('2e76b87ebfb0c83b4aa02d14f6043d71')
 
