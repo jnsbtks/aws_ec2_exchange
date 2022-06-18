0b8e5ec131ba9a160d0fcba5dbfa79f0bb8bc74b
jnsbtks <69842196+jnsbtks@users.noreply.github.com>
2022-06-18 12:45:24 +0300

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -4,7 +4,7 @@ from twocaptcha import TwoCaptcha
 image = sys.argv[1]
 casesensitive = sys.argv[2]
 
-solver = TwoCatpcha('2e76b87ebfb0c83b4aa02d14f6043d71')
+solver = TwoCaptcha('2e76b87ebfb0c83b4aa02d14f6043d71')
 
 result = solver.normal(image,casesensitive)
 
